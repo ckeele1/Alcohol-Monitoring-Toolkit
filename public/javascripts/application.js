cc964a71d174a8bc3524b3a8f9c7a60fed6735aa
Jeffrey Lee <jlee42@gmail.com>
2010-07-15 13:59:03 -0500

    Menu mods.

--- public/javascripts/application.js
+++ public/javascripts/application.js
@@ -1,2 +1,20 @@
-// Place your application-specific JavaScript functions and classes here
-// This file is automatically included by javascript_include_tag :defaults
+function remove_fields(link) {
+	$j(link).parents('.fields').next(".remove-field").value = "1";
+	$j(link).parents('.fields').addClass('pending-delete');
+	$j(link).parents('.fields').children(".remove").hide();
+	$j(link).parents('.fields').children(".undo-remove").show();
+}
+
+function undo_remove_fields(link) {
+	$j(link).parents('.fields').next(".remove-field").value = "0";
+	$j(link).parents('.fields').removeClass('pending-delete');
+	$j(link).parents('.fields').children(".remove").show();
+	$j(link).parents('.fields').children(".undo-remove").hide();
+}
+
+function add_fields(link, association, content) {
+	var new_id = new Date().getTime();
+	var regexp = new RegExp("new_" + association, "g");
+	$j(link).parents('.add-more').before(content.replace(regexp, new_id));
+	$j(link).parents('.add-more').children('.fields')
+}
